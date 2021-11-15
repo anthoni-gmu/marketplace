bd7fd69800aca8ec430ca2b1c650e3490a6d2412
anthoni portocarrero rodriguez <61163166+anthoni-gmu@users.noreply.github.com>
2021-11-15 03:44:07 -0500

    sidebarCategory

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CategoryView
+from .views import CategoryView,InfoView
 app_name = 'marketplace'
 
 urlpatterns = [
-    path('category/',CategoryView.as_view(),name='category')
+    path('category/',CategoryView.as_view(),name='category'),
+    path('info/',InfoView.as_view(),name='info'),
 ]
